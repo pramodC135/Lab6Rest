4b1f88b04ed54ee78402f44673f244a12eaff38d
pramodC135 <pramodmadhushan4@gmail.com>
2021-04-19 03:29:59 +0530

    API

---
+++ Lab6Rest/src/com/ItemService.java
@@ -0,0 +1,28 @@
+package com;
+
+import model.Item;
+
+//For REST Service
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+//For XML
+import org.jsoup.*; 
+import org.jsoup.parser.*; 
+import org.jsoup.nodes.Document; 
+
+@Path("/Items")
+public class ItemService {
+
+	Item itemObj = new Item();
+	
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String readItems()
+	{
+		return "Hello";
+	}
+}
