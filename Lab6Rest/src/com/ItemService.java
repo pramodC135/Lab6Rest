5119bfc4a40897c7689f499d01347cc6888beb67
pramodC135 <pramodmadhushan4@gmail.com>
2021-04-21 00:48:38 +0530

    items

--- Lab6Rest/src/com/ItemService.java
+++ Lab6Rest/src/com/ItemService.java
@@ -2,8 +2,11 @@ package com;
 
 import model.Item;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 //For REST Service
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -23,6 +26,21 @@ public class ItemService {
 	@Produces(MediaType.TEXT_HTML)
 	public String readItems()
 	{
-		return "Hello";
+		return itemObj.readItems();
 	}
+	
+	@POST
+	@Path("/") 
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED) 
+	@Produces(MediaType.TEXT_PLAIN) 
+	public String insertItem(@FormParam("itemCode") String itemCode, 
+	 @FormParam("itemName") String itemName, 
+	 @FormParam("itemPrice") String itemPrice, 
+	 @FormParam("itemDesc") String itemDesc) 
+	{ 
+	 String output = itemObj.insertItem(itemCode, itemName, itemPrice, itemDesc); 
+	return output; 
+	}
+
+
 }
