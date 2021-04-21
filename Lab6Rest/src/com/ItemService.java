cca883030c3f357c4988071fc5eac539e72d2e17
pramodC135 <pramodmadhushan4@gmail.com>
2021-04-21 15:44:10 +0530

    deletemethod

--- Lab6Rest/src/com/ItemService.java
+++ Lab6Rest/src/com/ItemService.java
@@ -3,6 +3,7 @@ package com;
 import model.Item;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 //For REST Service
 import javax.ws.rs.GET;
@@ -70,6 +71,25 @@ public class ItemService {
 			 
 			 return output; 
 	}
+	
+	
+	@DELETE
+	@Path("/") 
+	@Consumes(MediaType.APPLICATION_XML) 
+	@Produces(MediaType.TEXT_PLAIN) 
+	public String deleteItem(String itemData) 
+	{ 
+			//Convert the input string to an XML document
+			 Document doc = Jsoup.parse(itemData, "", Parser.xmlParser()); 
+			 
+			//Read the value from the element <itemID>
+			 String itemID = doc.select("itemID").text();
+			 
+			 String output = itemObj.deleteItem(itemID);
+			 
+			 return output; 
+	}
+
 
 
 }
